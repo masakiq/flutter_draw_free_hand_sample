7c4b95a0ac9bd8cb8ae72f93964c678a974190c1
masakiq <>
2021-04-18 23:43:04 +0900

    minimum

--- lib/main.dart
+++ lib/main.dart
@@ -15,42 +15,32 @@ class Writeable extends StatefulWidget {
 }
 
 class _WriteableState extends State<Writeable> {
-  List<List<double>> pressures = [[]];
   List<Offset> currentLine = [];
   List<List<Offset>> lines = [[]];
-  List<double> currentLinePressures = [];
-  double pressure;
-  Offset position;
-  Color color = Colors.black;
-  Painter painter;
-  CustomPaint paintCanvas;
+  Color color = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
-    painter = Painter(
-        lines: lines,
-        currentLine: currentLine,
-        pressures: pressures,
-        currentLinePressures: currentLinePressures,
-        color: color);
+    Painter painter = Painter(
+      lines: lines,
+      currentLine: currentLine,
+      color: color,
+    );
 
-    paintCanvas = CustomPaint(
+    CustomPaint paintCanvas = CustomPaint(
       painter: painter,
     );
 
     return Listener(
       onPointerMove: (details) {
         setState(() {
-          currentLinePressures.add(details.pressure);
           currentLine.add(details.localPosition);
         });
       },
       onPointerUp: (details) {
         setState(() {
           lines.add(currentLine.toList());
-          pressures.add(currentLinePressures.toList());
           currentLine.clear();
-          currentLinePressures.clear();
         });
       },
       child: paintCanvas,
@@ -59,18 +49,15 @@ class _WriteableState extends State<Writeable> {
 }
 
 class Painter extends CustomPainter {
-  Painter(
-      {@required this.lines,
-      @required this.currentLine,
-      @required this.color,
-      @required this.pressures,
-      @required this.currentLinePressures});
+  Painter({
+    @required this.lines,
+    @required this.currentLine,
+    @required this.color,
+  });
 
   final List<List<Offset>> lines;
   final List<Offset> currentLine;
   final Color color;
-  final List<List<double>> pressures;
-  final List<double> currentLinePressures;
 
   double scalePressures = 10;
   Paint paintStyle = Paint();
@@ -87,17 +74,23 @@ class Painter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     // Paint line currently being drawn (points added since pointer was
     // last lifted)
+    paintStyle.color = color;
+    paintStyle.strokeWidth = 3;
     for (int i = 0; i < currentLine.length - 1; i++) {
-      paintStyle.strokeWidth = currentLinePressures[i] * scalePressures;
       canvas.drawPoints(
-          PointMode.lines, [currentLine[i], currentLine[i + 1]], paintStyle);
+        PointMode.lines,
+        [currentLine[i], currentLine[i + 1]],
+        paintStyle,
+      );
     }
     // Paint all completed lines drawn since app start
     for (int i = 0; i < lines.length; i++) {
       for (int j = 0; j < lines[i].length - 1; j++) {
-        paintStyle.strokeWidth = pressures[i][j] * scalePressures;
         canvas.drawPoints(
-            PointMode.lines, [lines[i][j], lines[i][j + 1]], paintStyle);
+          PointMode.lines,
+          [lines[i][j], lines[i][j + 1]],
+          paintStyle,
+        );
       }
     }
   }
